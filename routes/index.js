979b0db0874ac4a872af88cc1dade0535073972c
jordan larcher <jordan.larcher.lds@gmail.com>
2025-05-10 21:11:21 -0300

    updating contacts route

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 //router.get('/', (req, res) => {res.send('Hello world');})
 
-router.use('/users', require('./contacts'));
+router.use('/contacts', require('./contacts'));
 
 
 module.exports = router;
